64df98e2722474a0d2424b54a3dc112ba3523e8e
owhiroro cleave <owhiroroeghele@gmail.com>
2019-02-15 13:29:50 +0100

    -added endpoints for adding a meal option

--- API/Dummydb/dummydb.js
+++ API/Dummydb/dummydb.js
@@ -28,5 +28,4 @@ export default {
       price: '1,500',
     },
   ],
-}
-;
+};
--- API/route/mealRoute.js
+++ API/route/mealRoute.js
@@ -5,5 +5,6 @@ import { control } from '../controller/mealController';
 const app = Express.Router();
 
 app.get('/api/v1/meals/', control.getAllMeals);
+app.post('/api/v1/meals', control.addMeal);
 
 export default app;
