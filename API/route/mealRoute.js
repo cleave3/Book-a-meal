27fd17dd9f05423a2f3deff64c608ae0e636be63
owhiroro cleave <owhiroroeghele@gmail.com>
2019-02-15 14:24:58 +0100

    added endpoint for updating a meal option

--- API/route/mealRoute.js
+++ API/route/mealRoute.js
@@ -7,5 +7,6 @@ const app = Express.Router();
 app.get('/api/v1/meals/', control.getAllMeals);
 app.post('/api/v1/meals', control.addMeal);
 app.get('/api/v1/meals/:id', control.getAmeal);
+app.put('/api/v1/meals/:id', control.updateAmeal);
 
 export default app;
