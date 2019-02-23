b3f3bcde3a766162ba6d5b1ecca72c7be8fb1edf
owhiroro cleave <owhiroroeghele@gmail.com>
2019-02-23 14:23:51 +0100

    refactor index.js

--- API/index.js
+++ API/index.js
@@ -10,7 +10,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(Route);
+app.use('/api/v1', Route);
+
+app.get('/api/v1', (req, res) => res.status(200).json({
+  message: 'this is the application home page'
+}));
+
+app.route('/*').all((req, res) => res.status(404).json({
+  status: 404,
+  error: '404 Route not found'
+}));
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
