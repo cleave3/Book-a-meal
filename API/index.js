0ae386b028786fc0f2a475c367dcb98a24f9d872
owhiroro cleave <owhiroroeghele@gmail.com>
2019-02-24 06:33:08 +0100

    added app get to index.js,add routeTest.js

--- API/index.js
+++ API/index.js
@@ -10,15 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use('/api/v1', Route);
+app.use( Route);
 
-app.get('/api/v1', (req, res) => res.status(200).json({
-  message: 'this is the application home page'
-}));
-
-app.route('/*').all((req, res) => res.status(404).json({
-  status: 404,
-  error: '404 Route not found'
+app.get('/', (req, res) => res.status(200).json({
+  status: 200,
+  message: 'App is running',
 }));
 
 app.listen(PORT, () => {
